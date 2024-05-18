b3c3fdf7e1e6290463f477730b272351c94f5d48
Ganesh sahu <sahug8279@gmail.com>
2024-05-18 15:58:06 +0530

    port establishment and basic routes

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log("welcome to ebook apis");
+import app from "./src/app";
+
+const startServer =()=>{
+    const port = process.env.PORT || 3000;
+
+    app.listen(port,()=>{
+        console.log(`listening on port : ${port}`);
+    });
+    // port with the callback it starts when the metod is being called
+    // it also indicates the port is also started running
+}
+
+startServer();
---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import  express  from "express";
+
+
+const app= express();
+
+// routes  , callbecks => (req, res, next) => {}
+app.get('/',(req, res, next) => {
+
+})
+
+
+export default app;
